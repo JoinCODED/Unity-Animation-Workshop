38e642e3e6bae5dc90c4346c54bb4e1003fe8c22
Huss <hussein@joincoded.com>
2019-10-22 18:18:25 +0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("space")){
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", true);
+            rb.AddForce(transform.forward*10000);
+        } else {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity-Animation-Workshop
+ 
